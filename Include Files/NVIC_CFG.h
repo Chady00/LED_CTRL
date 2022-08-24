6e898bd46cfa6598ef2a8a8ebfada9fe93139bae
Chady00 <84717550+Chady00@users.noreply.github.com>
2022-08-24 20:44:43 +0200

    Add files via upload

---
+++ Include Files/NVIC_CFG.h
@@ -0,0 +1,22 @@
+#ifndef NVIC_CFG_H
+#define NVIC_CFG_H
+
+
+/**********************************************************************
+*   //priority type
+*		NVIC_GROUPING_TYPE_XXX    		4
+*		NVIC_GROUPING_TYPE_XXY    		5
+*		NVIC_GROUPING_TYPE_XYY    		6
+*		NVIC_GROUPING_TYPE_YYY    		7
+*
+***************************************************************************/
+
+
+#define NVIC_CFG_GROUPING_SUBGROUPINT_PRIORITY_SYSTEM 	NVIC_GROUPING_TYPE_XXX
+
+#define NVIC_CFG_NUMBER_OF_CONFIGURED_HANDLERS					1
+
+
+
+
+#endif
